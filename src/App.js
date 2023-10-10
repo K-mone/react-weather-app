130a1959c4b793993d0519ed404ee1588d5d9ae0
K-Mone <kmw2547@gmail.com>
2023-10-10 22:30:54 +0630

    Updated the footer

--- src/App.js
+++ src/App.js
@@ -8,6 +8,15 @@ function App() {
         <p>🤡</p>
         <Weather />
       </header>
+      <footer>
+        This project was created by K-Mone. This is
+        <a href="https://github.com/K-mone/react-weather-app">
+          {" "}
+          github repo
+        </a>{" "}
+        and deploy it on{" "}
+        <a href="https://sparkling-malabi-aecf6d.netlify.app/"> Netlify</a>
+      </footer>
     </div>
   );
 }
